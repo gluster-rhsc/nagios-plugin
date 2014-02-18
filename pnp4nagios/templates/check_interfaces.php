12a48f42cf882c85526f555c9ab294dcfae05c3d
Bala.FA <barumuga@redhat.com>
2014-02-18 15:04:44 +0530

    network: remove rxpck/txpck graph and update color code
    Signed-off-by: Bala.FA <barumuga@redhat.com>

--- pnp4nagios/templates/check_interfaces.php
+++ pnp4nagios/templates/check_interfaces.php
@@ -32,32 +32,18 @@ $interface_count = $c / $VALUE_COUNT;
 for ($i = 0; $i < $interface_count; $i++) {
     $index = ($i * $VALUE_COUNT) + 1;
 
-    list ($interface, $data_type) = explode (".", $name[$index]);
-    $opt[$index] = "--vertical-label Load -l0  --title \"Network Interface Load for $hostname / $interface\" ";
-    $ds_name[$index] = "$interface:: $data_type and ";
-
-    $def[$index]  = rrd::def ($interface . $data_type, $RRDFILE[$index], $DS[$index], "AVERAGE");
-    $def[$index] .= rrd::area ($interface . $data_type, "#ff0000", $data_type);
-    $def[$index] .= rrd::gprint ($interface . $data_type, array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-
-    list ($interface, $data_type) = explode (".", $name[$index+1]);
-    $ds_name[$index] .= "$data_type";
-    $def[$index] .= rrd::def ($interface . $data_type, $RRDFILE[$index+1], $DS[$index+1], "AVERAGE");
-    $def[$index] .= rrd::area ($interface . $data_type, "#EACC00", $data_type);
-    $def[$index] .= rrd::gprint ($interface . $data_type, array("LAST", "AVERAGE", "MAX"), "%6.2lf");
-
     list ($interface, $data_type) = explode (".", $name[$index+2]);
     $opt[$index+1] = "--vertical-label Load -l0  --title \"Network Interface Load for $hostname / $interface\" ";
     $ds_name[$index+1] = "$interface:: $data_type and ";
 
     $def[$index+1]  = rrd::def ($interface . $data_type, $RRDFILE[$index+2], $DS[$index+2], "AVERAGE");
-    $def[$index+1] .= rrd::area ($interface . $data_type, "#ff0000", $data_type);
+    $def[$index+1] .= rrd::area ($interface . $data_type, "#ADD8E6", $data_type);
     $def[$index+1] .= rrd::gprint ($interface . $data_type, array("LAST", "AVERAGE", "MAX"), "%6.2lf");
 
     list ($interface, $data_type) = explode (".", $name[$index+3]);
     $ds_name[$index+1] .= "$data_type";
     $def[$index+1] .= rrd::def ($interface . $data_type, $RRDFILE[$index+3], $DS[$index+3], "AVERAGE");
-    $def[$index+1] .= rrd::area ($interface . $data_type, "#EACC00", $data_type) ;
+    $def[$index+1] .= rrd::area ($interface . $data_type, "#6CBB3C", $data_type) ;
     $def[$index+1] .= rrd::gprint ($interface . $data_type, array("LAST", "AVERAGE", "MAX"), "%6.2lf");
 }
 ?>

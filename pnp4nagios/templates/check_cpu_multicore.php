62190ef65ddd6a10801ea2276c7a7279cc1efcca
Nishanth Thomas <nthomas@redhat.com>
2014-02-20 19:00:01 +0530

    Updated the color code
    Signed-off-by: Nishanth Thomas <nthomas@redhat.com>

--- pnp4nagios/templates/check_cpu_multicore.php
+++ pnp4nagios/templates/check_cpu_multicore.php
@@ -22,32 +22,34 @@ $def[1] .= "DEF:user_cpu_in=$RRDFILE[1]:$DS[4]:AVERAGE " ;
 $def[1] .= "DEF:idle_cpu_in=$RRDFILE[1]:$DS[5]:AVERAGE " ;
 
 $def[1] .= "CDEF:user_cpu_out=user_cpu_in ";
-$def[1] .= "AREA:user_cpu_out#000080:\"User\t\t\" ";
+$def[1] .= "AREA:user_cpu_out#ADD8E6:\"User\t\t\" ";
 $def[1]  .= rrd::gprint("user_cpu_out", array("LAST", "AVERAGE", "MAX"), "%6.2lf%%");
 
 $def[1] .= rrd::cdef("system_cpu_out", "system_cpu_in");
-$def[1] .= "STACK:system_cpu_out#C000CC:\"System\t\" ";
+$def[1] .= "STACK:system_cpu_out#6CBB3C:\"System\t\" ";
 $def[1]  .= rrd::gprint("system_cpu_in", array("LAST", "AVERAGE", "MAX"), "%6.2lf%%");
 
 $def[1] .= rrd::cdef("total_cpu_out", "total_cpu_in,user_cpu_in,-,system_cpu_in,-");
-$def[1] .= "STACK:total_cpu_out#0000FF80:\"Total\t\t\" ";
+$def[1] .= "STACK:total_cpu_out#D462FF:\"Total\t\t\" ";
 $def[1]  .= rrd::gprint("total_cpu_in", array("LAST", "AVERAGE", "MAX"), "%6.2lf%%");
 
 $def[1] .= "CDEF:idle_cpu_out=idle_cpu_in ";
-$def[1] .= "STACK:idle_cpu_out#CCCCCC:\"Idle\t\t\" ";
+$def[1] .= "STACK:idle_cpu_out#C0C0C0:\"Idle\t\t\" ";
 $def[1]  .= rrd::gprint("idle_cpu_in", array("LAST", "AVERAGE", "MAX"), "%6.2lf%%");
 
 if ($WARN[2] != ""){
-  $def[1] .= "HRULE:$WARN[2]#FFFF00:\"Warning   \" ";
+  $def[1] .= "LINE2:$WARN[2]#FFA500:\"Warning   \" ";
 }
 if ($CRIT[2] != ""){              
-  $def[1] .= "HRULE:$CRIT[2]#FF0000:\"Critical  \" ";
+  $def[1] .= "LINE2:$CRIT[2]#FF0000:\"Critical  \" ";
 }
 
 # Graph Per-Core CPU usage
 $def_n=2;
 $index = 6;
 $no_cpu=$ACT[1];
+
+if($no_cpu>1)
 
 for( $cpu_n=0; $cpu_n<$no_cpu; $cpu_n++) {
     $def[$def_n]='';
@@ -65,26 +67,26 @@ for( $cpu_n=0; $cpu_n<$no_cpu; $cpu_n++) {
     $index += 1;
 
     $def[$def_n] .= "CDEF:user_cpu_out=user_cpu_in ";
-    $def[$def_n] .= "AREA:user_cpu_out#000080:\"User\t\t\" ";
+    $def[$def_n] .= "AREA:user_cpu_out#ADD8E6:\"User\t\t\" ";
     $def[$def_n]  .= rrd::gprint("user_cpu_out", array("LAST", "AVERAGE", "MAX"), "%6.2lf%%");
 
     $def[$def_n] .= rrd::cdef("system_cpu_out", "system_cpu_in");
-    $def[$def_n] .= "STACK:system_cpu_out#C000CC:\"System\t\" ";
+    $def[$def_n] .= "STACK:system_cpu_out#6CBB3C:\"System\t\" ";
     $def[$def_n]  .= rrd::gprint("system_cpu_in", array("LAST", "AVERAGE", "MAX"), "%6.2lf%%");
 
     $def[$def_n] .= rrd::cdef("total_cpu_out", "total_cpu_in,user_cpu_in,-,system_cpu_in,-");
-    $def[$def_n] .= "STACK:total_cpu_out#0000FF80:\"Total\t\t\" ";
+    $def[$def_n] .= "STACK:total_cpu_out#D462FF:\"Total\t\t\" ";
     $def[$def_n]  .= rrd::gprint("total_cpu_in", array("LAST", "AVERAGE", "MAX"), "%6.2lf%%");
 
     $def[$def_n] .= "CDEF:idle_cpu_out=idle_cpu_in ";
-    $def[$def_n] .= "STACK:idle_cpu_out#CCCCCC:\"Idle\t\t\" ";
+    $def[$def_n] .= "STACK:idle_cpu_out#C0C0C0:\"Idle\t\t\" ";
     $def[$def_n]  .= rrd::gprint("idle_cpu_in", array("LAST", "AVERAGE", "MAX"), "%6.2lf%%");
 
     if ($WARN[$index_of_threshold_val] != ""){
-        $def[$def_n] .= "HRULE:$WARN[$index_of_threshold_val]#FFFF00:\"Warning   \" ";
+        $def[$def_n] .= "LINE2:$WARN[$index_of_threshold_val]#FFFF00:\"Warning   \" ";
     }
     if ($CRIT[$index_of_threshold_val] != ""){              
-        $def[$def_n] .= "HRULE:$CRIT[$index_of_threshold_val]#FF0000:\"Critical  \" ";
+        $def[$def_n] .= "LINE2:$CRIT[$index_of_threshold_val]#FF0000:\"Critical  \" ";
     }
     $def_n++;
 }

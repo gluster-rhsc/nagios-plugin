8fa3dc8184d1792b862c8d58c074db29f24842c1
ndarshan <dnarayan@redhat.com>
2014-02-21 11:33:55 +0530

    Added licence information to php files
    Signed-off-by: ndarshan <dnarayan@redhat.com>

--- pnp4nagios/templates/check_memory.php
+++ pnp4nagios/templates/check_memory.php
@@ -1,8 +1,21 @@
 <?php
-#                                                                                                                         
-# Plugin: check_nrpe_cpu_core_stat                                                                                        
-# pst                                                                                                                     
-#                                                                                                                         
+#
+# check_interfaces -- template to generate RRD graph
+# Copyright (C) 2014 Red Hat Inc
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 $def[1]=""; $opt[1]=""; $ds_name[1]="";
 $opt[1] = "--vertical-label \"$UNIT[1]\"  -l 0 -u $MAX[1]   --title \"Memory usage for $hostname / $servicedesc\" --slope-mode -N";
--- pnp4nagios/templates/check_swap_usage.php
+++ pnp4nagios/templates/check_swap_usage.php
@@ -1,10 +1,25 @@
 <?php
 #
-# RRDtool Options
+# check_interfaces -- template to generate RRD graph
+# Copyright (C) 2014 Red Hat Inc
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
 $opt[1] = "-X 0 --vertical-label $UNIT[1] -l 0 -u $MAX[1] --title \"Swap usage $hostname / $servicedesc\" ";
-#
-#
-# Graphen Definitions
+
 $def[1] = "DEF:used_swap_in=$RRDFILE[1]:$DS[1]:AVERAGE " ;
 if ($MAX[1] != "") {
   $def[1] .= "AREA:$MAX[1]#ADD8E6:\"Free             \\n\" ";

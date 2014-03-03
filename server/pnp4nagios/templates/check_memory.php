d3c046fd49f71a86cf606b26c8e635518fefe3c2
ndarshan <dnarayan@redhat.com>
2014-03-03 18:05:11 +0530

    Moved unit conversion logic for memory and swap from plugin to template.
    Signed-off-by: ndarshan <dnarayan@redhat.com>

--- server/pnp4nagios/templates/check_memory.php
+++ server/pnp4nagios/templates/check_memory.php
@@ -18,7 +18,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 $def[1]=""; $opt[1]=""; $ds_name[1]="";
-$opt[1] = "--vertical-label \"$UNIT[1](Total:$MAX[1] GB)\"  -l 0 -u $MAX[1] -r  --title \"Memory usage for $hostname / $servicedesc\" --slope-mode -N";
+$upper_limit = round($MAX[1] / (1048576),2)
+$opt[1] = "--vertical-label \"$UNIT[1](Total:$upper_limit GB)\"  -l 0 -u $upper_limit -r  --title \"Memory usage for $hostname / $servicedesc\" --slope-mode -N";
 $ds_name[1] = "Memory Usage";
 
 
@@ -28,29 +29,31 @@ $def[1] .= "DEF:used_mem_in=$RRDFILE[1]:$DS[2]:AVERAGE " ;
 $def[1] .= "DEF:buffer_mem_in=$RRDFILE[1]:$DS[3]:AVERAGE " ;
 $def[1] .= "DEF:cached_mem_in=$RRDFILE[1]:$DS[4]:AVERAGE " ;
 
-$def[1] .= "CDEF:used_mem_out=used_mem_in ";
+$def[1] .= rrd::cdef("used_mem_out","used_mem_in,1048576,/");
 $def[1] .= "LINE1:used_mem_out#0000ff:\"Used        \" ";
-$def[1] .= "GPRINT:used_mem_in:LAST:\"%8.2lf $UNIT[1] LAST \" ";
-$def[1] .= "GPRINT:used_mem_in:MAX:\"%8.2lf $UNIT[1] MAX \" ";
+$def[1] .= "GPRINT:used_mem_in:LAST:\"%8.2lf GB LAST \" ";
+$def[1] .= "GPRINT:used_mem_in:MAX:\"%8.2lf GB MAX \" ";
 $def[1] .= "GPRINT:used_mem_in" . ':AVERAGE:"%8.2lf GB AVERAGE \j" ';
 
-$def[1] .= "CDEF:buffer_mem_out=buffer_mem_in ";
+$def[1] .= rrd::cdef("buffer_mem_out","buffer_mem_in,1048576,/");
 $def[1] .= "LINE1:buffer_mem_out#800080:\"Buffer      \" ";
-$def[1] .= "GPRINT:buffer_mem_in:LAST:\"%8.2lf $UNIT[1] LAST \" ";
-$def[1] .= "GPRINT:buffer_mem_in:MAX:\"%8.2lf $UNIT[1] MAX \" ";
+$def[1] .= "GPRINT:buffer_mem_in:LAST:\"%8.2lf GB LAST \" ";
+$def[1] .= "GPRINT:buffer_mem_in:MAX:\"%8.2lf GB MAX \" ";
 $def[1] .= "GPRINT:buffer_mem_in" . ':AVERAGE:"%8.2lf GB AVERAGE \j" ';
 
-$def[1] .= "CDEF:cached_mem_out=cached_mem_in ";
+$def[1] .= rrd::cdef("cached_mem_out","cached_mem_in,1048576,/");
 $def[1] .= "LINE1:cached_mem_out#008000:\"Cached      \" ";
-$def[1] .= "GPRINT:cached_mem_in:LAST:\"%8.2lf $UNIT[1] LAST \" ";
-$def[1] .= "GPRINT:cached_mem_in:MAX:\"%8.2lf $UNIT[1] MAX \" ";
+$def[1] .= "GPRINT:cached_mem_in:LAST:\"%8.2lf GB LAST \" ";
+$def[1] .= "GPRINT:cached_mem_in:MAX:\"%8.2lf GB MAX \" ";
 $def[1] .= "GPRINT:cached_mem_in" . ':AVERAGE:"%8.2lf GB AVERAGE \j" ';
 
 if ($WARN[1] != ""){
-  $def[1] .= "LINE2:$WARN[1]#FFA500:\"Warning \\n\" ";
+  $WAR = $WARN[1] / 1048576 ;
+  $def[1] .= "LINE2:$WAR#FFA500:\"Warning \\n\" ";
 }
 if ($CRIT[1] != "") {
-  $def[1] .= "LINE2:$CRIT[1]#FF0000:\"Critical  \\n\" ";
+  $CRT = $CRIT[1] / 1048576 ;
+  $def[1] .= "LINE2:$CRT#FF0000:\"Critical  \\n\" ";
   }
 
 ?>
--- server/pnp4nagios/templates/check_swap_usage.php
+++ server/pnp4nagios/templates/check_swap_usage.php
@@ -17,22 +17,24 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-
-$opt[1] = "-X 0 --vertical-label \"$UNIT[1](Total:$MAX[1] GB)\" -l 0 -r -u $MAX[1] --title \"Swap usage $hostname / $servicedesc\" ";
+$upper_limit = round($MAX[1] / (1048576),2);
+$opt[1] = "-X 0 --vertical-label \"$UNIT[1](Total:$upper_limit GB)\" -l 0 -r -u $upper_limit --title \"Swap usage $hostname / $servicedesc\" ";
 $ds_name[1] = "Swap Usage";
 $def[1] = "DEF:used_swap_in=$RRDFILE[1]:$DS[1]:AVERAGE " ;
 
-$def[1] .= "CDEF:used_swap_out=used_swap_in ";
+$def[1] .= rrd::cdef("used_swap_out","used_swap_in,1048576,/");
 $def[1] .= "AREA:used_swap_out#3D1AA8:\"Used \" ";
 $def[1] .= "GPRINT:used_swap_out:LAST:\"%3.2lf GB LAST \" ";
 $def[1] .= "GPRINT:used_swap_out:MAX:\"%3.2lf GB MAX \" ";
 $def[1] .= "GPRINT:used_swap_out" . ':AVERAGE:"%3.2lf GB AVERAGE \j" ';
 
 if ($WARN[1] != ""){
-  $def[1] .= "LINE2:$WARN[1]#FFA500:\"Warning       \\n\" ";
+  $WAR = $WARN[1] / 1048576 ;
+  $def[1] .= "LINE2:$WAR#FFA500:\"Warning       \\n\" ";
 }
 if ($CRIT[1] != "") {
-  $def[1] .= "LINE2:$CRIT[1]#FF0000:\"Critical \\n\" ";
+  $CRT = $CRIT[1] / 1048576 ;
+  $def[1] .= "LINE2:$CRT#FF0000:\"Critical \\n\" ";
 }
 
 

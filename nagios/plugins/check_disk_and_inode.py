3f934f6727455865633834e2354b18eb5e8cbf38
Timothy Asir <tim.gluster@gmail.com>
2014-03-07 18:21:44 +0530

    Add test case for disk_and_inode_usage plugin
    Signed-off-by: Timothy Asir <tim.gluster@gmail.com>

--- nagios/plugins/check_disk_and_inode.py
+++ nagios/plugins/check_disk_and_inode.py
@@ -61,122 +61,130 @@ def appendStatus(lst, level, typ, device, mpath, usage):
     lst.append("%s:%s:%s;%s;%s" % (level, device, mpath, usage))
 
 
-parser = OptionParser()
-parser.add_option('-w', '--warning', action='store', type='int',
-                  dest='warn', help='Warning count in %', default=80)
-parser.add_option('-c', '--critical', action='store', type='int',
-                  dest='crit', help='Critical count in %', default=90)
-parser.add_option('-u', '--usage', action="store_true", dest='usage',
-                  help='Output disk and inode usage', default=False)
-parser.add_option('-l', '--lvm', action="store_true",
-                  dest='lvm', help='List lvm mounts', default=False)
-parser.add_option('-a', '--all', action="store_true",
-                  dest='all', help='List all mounts', default=False)
-parser.add_option('-n', '--ignore', action="store_true",
-                  dest='ignore', help='Ignore errors', default=False)
-parser.add_option('-i', '--include', action='append', type='string',
-                  dest='mountPath', help='Mount path', default=[])
-parser.add_option('-x', '--exclude', action="append", type='string',
-                  dest='exclude', help='Exclude disk')
-
-(options, args) = parser.parse_args()
-
-if len(args) > 2:
-    if args[0].isdigit() and args[1].isdigit():
-        warn = int(args[0])
-        crit = int(args[1])
-        options.mountPath = args[2:]
-    else:
-        warn = 80
-        crit = 90
-        options.mountPath = args
-else:
-    crit = options.crit
-    warn = options.warn
-
-if options.lvm:
-    searchQuery = "/dev/mapper"
-else:
-    searchQuery = "/"
-
-if not options.mountPath or options.lvm or options.all:
+def getMounts(searchQuery, excludeList=[]):
+    mountPaths = []
     f = open("/etc/mtab")
     for i in f.readlines():
         if i.startswith(searchQuery):
-            if not options.exclude:
-                options.mountPath.append(i.split()[0])
+            if not excludeList:
+                mountPaths.append(i.split()[0])
             else:
                 device = i.split()
                 if not device[0] in options.exclude and\
                    not device[1] in options.exclude:
-                    options.mountPath.append(device[0])
+                    mountPaths.append(device[0])
     f.close()
+    return mountPaths
 
-if not options.mountPath:
-    parser.print_help()
-    sys.exit(1)
 
-disk = []
-warnList = []
-critList = []
-diskList = []
-mounts = []
-level = -1
+def parse_input():
+    parser = OptionParser()
+    parser.add_option('-w', '--warning', action='store', type='int',
+                      dest='warn', help='Warning count in %', default=80)
+    parser.add_option('-c', '--critical', action='store', type='int',
+                      dest='crit', help='Critical count in %', default=90)
+    parser.add_option('-u', '--usage', action="store_true", dest='usage',
+                      help='Output disk and inode usage', default=False)
+    parser.add_option('-l', '--lvm', action="store_true",
+                      dest='lvm', help='List lvm mounts', default=False)
+    parser.add_option('-a', '--all', action="store_true",
+                      dest='all', help='List all mounts', default=False)
+    parser.add_option('-n', '--ignore', action="store_true",
+                      dest='ignore', help='Ignore errors', default=False)
+    parser.add_option('-i', '--include', action='append', type='string',
+                      dest='mountPath', help='Mount path', default=[])
+    parser.add_option('-x', '--exclude', action="append", type='string',
+                      dest='exclude', help='Exclude disk')
+    return parser.parse_args()
 
-for path in options.mountPath:
-    diskUsage, diskFree, used, avail, dev, mpath = getDisk(path,
-                                                           options.usage,
-                                                           options.lvm)
-    inodeUsage, inodeFree, iused, iavail, idev, ipath = getInode(path,
-                                                                 options.usage,
-                                                                 options.lvm)
-    if mpath in mounts:
-        continue
-    if not used or not iused:
-        if options.ignore:
+
+if __name__ == '__main__':
+    disk = []
+    warnList = []
+    critList = []
+    diskList = []
+    mounts = []
+    level = -1
+    (options, args) = parse_input()
+
+    if len(args) > 2:
+        if args[0].isdigit() and args[1].isdigit():
+            warn = int(args[0])
+            crit = int(args[1])
+            options.mountPath = args[2:]
+        else:
+            warn = 80
+            crit = 90
+            options.mountPath = args
+    else:
+        crit = options.crit
+        warn = options.warn
+
+    if options.lvm:
+        searchQuery = "/dev/mapper"
+    else:
+        searchQuery = "/"
+
+    if not options.mountPath or options.lvm or options.all:
+        options.mountPath = getMounts(searchQuery, options.exclude)
+
+    #if not options.mountPath:
+    #    parser.print_help()
+    #    sys.exit(1)
+
+    for path in options.mountPath:
+        diskUsage, diskFree, used, avail, dev, mpath = getDisk(path,
+                                                               options.usage,
+                                                               options.lvm)
+        inodeUsage, inodeFree, iused, iavail, idev, ipath = getInode(path,
+                                                                     options.usage,
+                                                                     options.lvm)
+        if mpath in mounts:
             continue
-        else:
-            sys.exit(3)
+        if not used or not iused:
+            if options.ignore:
+                continue
+            else:
+                sys.exit(3)
 
-    mounts.append(mpath)
-    if options.usage:
-        total = (float(used) + float(avail)) / 1000
-        itot = (float(iused) + float(iavail)) / 1000
-        disk.append("%s=%.1f;%.1f;%.1f;0;%.1f %s=%.1f;%.1f;%.1f;0;%.1f" % (
-            mpath, float(used)/1000, warn*total/100, crit*total/100, total,
-            ipath, float(iused)/1000, warn*itot/100, crit*itot/100, itot))
+        mounts.append(mpath)
+        if options.usage:
+            total = (float(used) + float(avail)) / 1000
+            itot = (float(iused) + float(iavail)) / 1000
+            disk.append("%s=%.1f;%.1f;%.1f;0;%.1f %s=%.1f;%.1f;%.1f;0;%.1f" % (
+                mpath, float(used)/1000, warn*total/100, crit*total/100, total,
+                ipath, float(iused)/1000, warn*itot/100, crit*itot/100, itot))
+        else:
+            disk.append("%s=%.2f;%s;%s;0;100 %s=%.2f;%s;%s;0;100" % (
+                mpath, diskUsage, warn, crit, ipath, inodeUsage, warn, crit))
+
+        if diskUsage >= crit or inodeUsage >= crit:
+            if diskUsage >= crit:
+                critList.append("crit:disk:%s;%s;%s" % (dev, mpath, diskUsage))
+            else:
+                critList.append("crit:inode:%s;%s;%s" % (idev, ipath, inodeUsage))
+            if not level > 1:
+                level = 2
+        elif (diskUsage >= warn and diskUsage < crit) or (
+                inodeUsage >= warn and inodeUsage < crit):
+            if diskUsage >= warn:
+                warnList.append("warn:disk:%s;%s;%s" % (dev, mpath, diskUsage))
+            else:
+                warnList.append("warn:inode:%s;%s;%s" % (idev, ipath, inodeUsage))
+            if not level > 0:
+                level = 1
+        else:
+            diskList.append("%s:%s" % (dev, mpath))
+
+    msg = " ".join(critList + warnList)
+    if not msg:
+        msg += " disks:mounts:(" + ",".join(diskList) + ")"
+
+    if 2 == level:
+        print "CRITICAL : %s | %s" % (msg, " ".join(disk))
+        sys.exit(2)
+    elif 1 == level:
+        print "WARNING : %s | %s" % (msg, " ".join(disk))
+        sys.exit(1)
     else:
-        disk.append("%s=%.2f;%s;%s;0;100 %s=%.2f;%s;%s;0;100" % (
-            mpath, diskUsage, warn, crit, ipath, inodeUsage, warn, crit))
-
-    if diskUsage >= crit or inodeUsage >= crit:
-        if diskUsage >= crit:
-            critList.append("crit:disk:%s;%s;%s" % (dev, mpath, diskUsage))
-        else:
-            critList.append("crit:inode:%s;%s;%s" % (idev, ipath, inodeUsage))
-        if not level > 1:
-            level = 2
-    elif (diskUsage >= warn and diskUsage < crit) or (
-            inodeUsage >= warn and inodeUsage < crit):
-        if diskUsage >= warn:
-            warnList.append("warn:disk:%s;%s;%s" % (dev, mpath, diskUsage))
-        else:
-            warnList.append("warn:inode:%s;%s;%s" % (idev, ipath, inodeUsage))
-        if not level > 0:
-            level = 1
-    else:
-        diskList.append("%s:%s" % (dev, mpath))
-
-msg = " ".join(critList + warnList)
-if not msg:
-    msg += " disks:mounts:(" + ",".join(diskList) + ")"
-
-if 2 == level:
-    print "CRITICAL : %s | %s" % (msg, " ".join(disk))
-    sys.exit(2)
-elif 1 == level:
-    print "WARNING : %s | %s" % (msg, " ".join(disk))
-    sys.exit(1)
-else:
-    print "OK : %s | %s" % (msg, " ".join(disk))
-    sys.exit(0)
+        print "OK : %s | %s" % (msg, " ".join(disk))
